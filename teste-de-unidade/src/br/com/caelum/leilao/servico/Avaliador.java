d54162f8fa4c00c1695d9b5eb2e050f682d3bfcd
Jorge Hech <diordiaum@hotmail.com>
2019-07-03 13:08:27 -0300

    criando o menor lance e testando

--- teste-de-unidade/src/br/com/caelum/leilao/servico/Avaliador.java
+++ teste-de-unidade/src/br/com/caelum/leilao/servico/Avaliador.java
@@ -4,17 +4,26 @@ import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 
 public class Avaliador {
-	// constando que guarda o menor número double
+	// constante que guarda o menor número double
 	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
+	// constante que guarda o maior número double
+	private double menorDeTodos = Double.POSITIVE_INFINITY;
 
 	public void avalia(Leilao leilao) {
 		for (Lance lance : leilao.getLances()) {
 			if (lance.getValor() > maiorDeTodos )
 				maiorDeTodos = lance.getValor();
+			else if(lance.getValor() < menorDeTodos)
+				menorDeTodos = lance.getValor();
 		}
+			
 	}
 	
 	public double getMaiorLance() {
 		return maiorDeTodos;
+	}
+	
+	public double getMenorLance() {
+		return menorDeTodos;
 	}
 }
