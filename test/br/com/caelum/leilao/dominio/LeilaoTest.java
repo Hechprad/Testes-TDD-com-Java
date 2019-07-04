13f108268282ea66445415c61226b556f2a71826
Jorge Hech <diordiaum@hotmail.com>
2019-07-04 12:55:42 -0300

    teste de lances

--- test/br/com/caelum/leilao/dominio/LeilaoTest.java
+++ test/br/com/caelum/leilao/dominio/LeilaoTest.java
@@ -1,5 +1,32 @@
 package br.com.caelum.leilao.dominio;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class LeilaoTest {
 
+	@Test
+	public void deveReceberUmLance() {
+		Leilao leilao = new Leilao("Monitor 21 pol");
+		assertEquals(0, leilao.getLances().size());
+		
+		leilao.propoe(new Lance(new Usuario("Adebaior"), 500.0));
+
+		assertEquals(1, leilao.getLances().size());
+		assertEquals(500.0, leilao.getLances().get(0).getValor(), 0.00001);
+	}
+	
+	@Test
+	public void deveReceberVariosLances() {
+		Leilao leilao = new Leilao("Monitor 40 pol");
+		leilao.propoe(new Lance(new Usuario("Steve Jobs"), 2000.0));
+		leilao.propoe(new Lance(new Usuario("Steve Wozniak"), 3000.0));
+		leilao.propoe(new Lance(new Usuario("Adebaior"), 500.0));
+		
+		assertEquals(3, leilao.getLances().size());
+		assertEquals(2000.0, leilao.getLances().get(0).getValor(), 0.00001);
+		assertEquals(3000.0, leilao.getLances().get(1).getValor(), 0.00001);
+		assertEquals(500.0, leilao.getLances().get(2).getValor(), 0.00001);
+	}
 }
