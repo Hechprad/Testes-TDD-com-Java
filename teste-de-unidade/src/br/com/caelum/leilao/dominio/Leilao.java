41c44d612ad948012f6603477f77366f0aae7308
Jorge Hech <diordiaum@hotmail.com>
2019-07-04 13:35:07 -0300

    novo teste, nova funcionalidade, roda o teste novamente, refatora o código, testa de novo

--- teste-de-unidade/src/br/com/caelum/leilao/dominio/Leilao.java
+++ teste-de-unidade/src/br/com/caelum/leilao/dominio/Leilao.java
@@ -15,8 +15,22 @@ public class Leilao {
 	}
 	
 	public void propoe(Lance lance) {
-		if(lances.isEmpty() || !lances.get(lances.size()-1).getUsuario().equals(lance.getUsuario()))
+		int total = qtdDeLancesDo(lance.getUsuario());
+		
+		if(lances.isEmpty() || (!ultimoLanceDado().getUsuario().equals(lance.getUsuario())) && total < 5)
 		lances.add(lance);
+	}
+
+	private int qtdDeLancesDo(Usuario usuario) {
+		int total = 0;
+		for (Lance l : lances) {
+			if(l.getUsuario().equals(usuario)) total++;
+		}
+		return total;
+	}
+
+	private Lance ultimoLanceDado() {
+		return lances.get(lances.size()-1);
 	}
 
 	public String getDescricao() {
